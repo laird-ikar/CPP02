275d68155006966d28ff46792089e2e46182e341
laird-ikar <bguyot@student.42mulhouse.fr>
2022-05-20 07:13:55 +0200

    ex02

--- ex02/Fixed.cpp
+++ ex02/Fixed.cpp
@@ -6,6 +6,57 @@
 /*   By: bguyot <bguyot@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 08:10:45 by bguyot            #+#    #+#             */
-/*   Updated: 2022/05/19 08:12:26 by bguyot           ###   ########.fr       */
+/*   Updated: 2022/05/20 07:13:48 by bguyot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef FIXED_H
+#define FIXED_H
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+class Fixed
+{
+private:
+	const static int	nb_fract_bit = 0x8;
+	int					raw_bits;
+
+public:
+	Fixed(void);
+	Fixed(Fixed const &src);
+	~Fixed(void);
+
+	Fixed(const int n);
+	Fixed(const float f);
+
+	Fixed				&operator=(Fixed const &rhs);
+	Fixed				&operator++(void);
+	Fixed				&operator--(void);
+	Fixed				operator++(int);
+	Fixed				operator--(int);
+	bool				operator>(Fixed const &f);
+	bool				operator<(Fixed const &f);
+	bool				operator>=(Fixed const &f);
+	bool				operator<=(Fixed const &f);
+	bool				operator==(Fixed const &f);
+	bool				operator!=(Fixed const &f);
+	Fixed				operator+(Fixed const &f);
+	Fixed				operator-(Fixed const &f);
+	Fixed				operator*(Fixed const &f);
+	Fixed				operator/(Fixed const &f);
+
+	int					getRawBits(void) const;
+	void				setRawBits(int const raw);
+	float				toFloat(void) const;
+	int					toInt(void) const;
+	static Fixed		&min(Fixed &f1, Fixed &f2);
+	static Fixed		&max(Fixed &f1, Fixed &f2);
+	static const Fixed	&min(const Fixed &f1, const Fixed &f2);
+	static const Fixed	&max(const Fixed &f1, const Fixed &f2);
+};
+
+std::ostream	&operator<<(std::ostream &o, Fixed const &f);
+
+#endif
