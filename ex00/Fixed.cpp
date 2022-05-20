dceb8de07e225a1e9bc910aea898593436f5ade1
laird-ikar <bguyot@student.42mulhouse.fr>
2022-05-20 07:08:44 +0200

    ex00

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -6,6 +6,49 @@
 /*   By: bguyot <bguyot@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 08:10:45 by bguyot            #+#    #+#             */
-/*   Updated: 2022/05/19 08:10:55 by bguyot           ###   ########.fr       */
+/*   Updated: 2022/05/20 07:08:08 by bguyot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+#include <string>
+
+Fixed::Fixed(void)
+{
+	std::cout << "Default constructor called" << std::endl;
+	this->raw_bits = 0;
+	return ;
+}
+
+Fixed::Fixed(Fixed const &src)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+	return ;
+}
+
+Fixed	&Fixed::operator=(Fixed const &rhs)
+{
+	std::cout << "Copy assignement operator called" << std::endl;
+	this->raw_bits = rhs.getRawBits();
+	return *this;
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+	return ;
+}
+
+int	Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return this->raw_bits;
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	this->raw_bits = raw;
+}
--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -6,6 +6,32 @@
 /*   By: bguyot <bguyot@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 08:10:47 by bguyot            #+#    #+#             */
-/*   Updated: 2022/05/19 08:10:54 by bguyot           ###   ########.fr       */
+/*   Updated: 2022/05/20 07:08:15 by bguyot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef FIXED_H
+#define FIXED_H
+
+#include <iostream>
+#include <string>
+
+class Fixed
+{
+private:
+	const static int	nb_fract_bit = 0x8;
+
+	int					raw_bits;
+
+public:
+	Fixed(void);
+	Fixed(Fixed const &src);
+	~Fixed(void);
+
+	Fixed	&operator=(Fixed const &rhs);
+
+	int	getRawBits(void) const;
+	void setRawBits(int const raw);
+};
+
+#endif
